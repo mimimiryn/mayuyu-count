9f3c2ca8ef86ca2b51b790483f4ba1b45f9c6bf0
miryon <MiryonKim@kin-no-MacBook-Air.local>
2015-06-05 19:53:12 +0900

    Initial Commit

---
+++ まゆゆカウント/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  まゆゆカウント
+//
+//  Created by miryon on 2015/06/05.
+//  Copyright (c) 2015年 miryon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
